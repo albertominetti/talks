4ae751dd68a8a574772072fbf7e516546adf6739
Alberto Minetti <alberto.minetti@rumbo.com>
2017-06-10 12:41:41 +0200

    enable JSF in a spring-boot application

--- src/main/java/it/minetti/TalksApplication.java
+++ src/main/java/it/minetti/TalksApplication.java
@@ -1,10 +1,13 @@
 package it.minetti;
 
-import it.minetti.controller.RestTalkController;
-import it.minetti.logic.TalksService;
+import it.minetti.controller.model.RestTalk;
+import it.minetti.persistence.model.Talk;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.builders.PathSelectors;
@@ -13,13 +16,16 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import javax.faces.webapp.FacesServlet;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-@ComponentScan(basePackageClasses = TalksService.class)
 @SpringBootApplication
+@EnableAutoConfiguration
 @EnableSwagger2
-@ComponentScan(basePackageClasses = {RestTalkController.class})
+@ComponentScan(basePackageClasses = TalksApplication.class)
+@EntityScan(basePackageClasses = { Talk.class, RestTalk.class })
+
 public class TalksApplication {
 
   public static void main(String[] args) {
@@ -37,6 +43,26 @@ public class TalksApplication {
       //.useDefaultResponseMessages(false)
       .directModelSubstitute(LocalDateTime.class, String.class)
       .directModelSubstitute(LocalDate.class, String.class);
+  }
+
+  @Bean
+  public FacesServlet facesServlet() {
+    return new FacesServlet();
+  }
+
+  @Bean
+  public ServletRegistrationBean facesServletRegistration() {
+    ServletRegistrationBean registration = new ServletRegistrationBean(facesServlet(), "*.xhtml");
+    registration.setName("FacesServlet");
+    return registration;
+  }
+
+  @Bean
+  public ServletContextInitializer initializer() {
+    return servletContext -> {
+      servletContext.setInitParameter("primefaces.THEME", "bootstrap");
+      servletContext.setInitParameter("javax.faces.PROJECT_STAGE", "Development"); //TODO just for dev
+    };
   }
 
 }
