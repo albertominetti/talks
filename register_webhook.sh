557d1cc0b5e841d39df41619b311d16e282b6737
Alberto Minetti <alberto.minetti@rumbo.com>
2017-06-10 13:11:13 +0200

    check token in bash register hook

--- register_webhook.sh
+++ register_webhook.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
-TELEGRAM_TOKEN=''
+
+if [[ -z $TELEGRAM_TOKEN ]]; then
+  echo "You must specify the TELEGRAM_TOKEN variable"
+  exit 1;
+fi
 curl -F "url=https://talks-minetti.herokuapp.com/telegram" 'https://api.telegram.org/bot${TELEGRAM_TOKEN}/setWebhook'
